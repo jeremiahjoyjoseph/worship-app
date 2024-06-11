0e0a68036f9a21fb92a8cc33763a5f2b3201fcb3
Jeremiah Joy Joseph <jeremiahjoyjoseph97@gmail.com>
2024-06-12 05:27:01 +0530

    new: submit availability page

--- src/pages/submitAvailability/submitAvailability.tsx
+++ src/pages/submitAvailability/submitAvailability.tsx
@@ -1,13 +1,13 @@
 import { type FC } from "react";
 
-const DashboardPage: FC = function () {
+const SubmitAvailability: FC = function () {
   return (
     <div className="px-4 pt-6">
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
-        Dashboard
+        Submit Availability
       </h1>
     </div>
   );
 };
 
-export default DashboardPage;
+export default SubmitAvailability;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import DashboardPage from "../pages";
+import SubmitAvailability from "../pages/submitAvailability/submitAvailability";
 
 const AppRouter: FC = () => {
   return (
     <Routes>
       <Route path="/" element={<DashboardPage />} index />
+      <Route
+        path="/submit-availability/:rosterId"
+        element={<SubmitAvailability />}
+        index
+      />
     </Routes>
   );
 };
